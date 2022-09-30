bad68b391ddd0acb98ecb3313e1ca64152546467
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-09-30 09:59:57 -0300

    Verificando objeto address antes de salvar

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,4 +1,6 @@
 import Address from "../models/address.js";
+import * as addressService from '../services/address-service.js';
+import * as listController from './list-controller.js'
 
 function State() {
 
@@ -31,5 +33,87 @@ export function init() {
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
 
-    console.log(state);
-} 
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.inputNumber.addEventListener('keyup', handleInputNumberKeyup);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+    state.inputCep.addEventListener('change', handleInputCepChange);
+
+}
+
+function handleInputNumberKeyup(event){
+    state.address.number = event.target.value;
+}
+
+async function handleInputCepChange(event) {
+    const cep = event.target.value;
+
+    try {
+        const address = await addressService.findByCep(cep);
+
+        state.inputCity.value = address.city;
+        state.inputStreet.value = address.street;
+        state.address = address;
+    
+        setFormError("cep", "");
+        state.inputNumber.focus();
+    }
+    catch (e) {
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um CEP válido");
+    }
+}
+
+function handleBtnSaveClick(event) {
+    event.preventDefault();
+
+    const errors = addressService.getErrors(state.address);
+
+    // const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+    
+    const keys = Object.keys(errors);
+    
+    if(keys.length > 0) {
+        for(let i = 0; i < keys.length; i++) {
+            setFormError(keys[i], errors[keys[i]]);
+        }
+    }
+    else {
+        listController.addCard(state.address);
+        clearForm();
+    }
+}
+
+function handleInputNumberChange(event) {
+    if (event.target.value == "") {
+        setFormError("number", "Campo requerido");
+    }
+    else {
+        setFormError("number", "");
+    }
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm() {
+    state.inputCep.value = "";
+    state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.address = new Address();
+
+    state.inputCep.focus();
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
+}
